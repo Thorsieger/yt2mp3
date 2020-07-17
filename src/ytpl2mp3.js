232f579d2cb41822460cfcb3d1754454d364d0eb
Thorsieger <mertz.thomas@live.fr>
2020-07-17 22:26:46 +0200

    affichage de l'avancement de telechargement et gestion des metadata

--- src/ytpl2mp3.js
+++ src/ytpl2mp3.js
@@ -10,26 +10,24 @@ function download(){
   const startTime = Date.now();
   const link = document.getElementById('playlist-id').value;
 
-  if(link==undefined)return document.getElementById('text-out').textContent +="/!\\Le programme prend en argument le lien de la playlist/!\\\n";
+  if(link==undefined)return document.getElementById('text-out').innerHTML ="/!\\Le programme prend en argument le lien de la playlist/!\\";
 
   if(ytpl.validateURL(link)){
     ytpl(link, { limit: Infinity } , function(err, playlist) {
-      console.log(playlist,err);
-        if(err) return document.getElementById('text-out').textContent +=err.name + " : " + err.message + "\n";
-        document.getElementById('text-out').textContent +="Téléchargement de la playlist : " + playlist.title + "\n";
-        document.getElementById('text-out').textContent +="Nombre de piste audio à télécharger : " + playlist.total_items + "\n\n";
+        if(err) return document.getElementById('text-out').innerHTML +=err.name + " : " + err.message + "<br>";
+        document.getElementById('text-out').innerHTML ="Téléchargement de la playlist : " + playlist.title + "<br>";
+        document.getElementById('text-out').innerHTML +="Nombre de piste audio à télécharger : " + playlist.total_items + "<br><br>";
         if (!fs.existsSync(path + '/' +playlist.title))fs.mkdirSync(path + "/"+ playlist.title);
         dl_track(playlist,0);
       });
   }else{
-    document.getElementById('text-out').textContent +="Erreur lien non valide\n";
+    document.getElementById('text-out').innerHTML +="Erreur lien non valide<br>";
   }
   }
 
 const onProgress = (chunkLength, downloaded, total) => {
   const percent = downloaded / total;
-  document.getElementById('text-out').textContent +=`${(percent * 100).toFixed(2)}% downloaded `;
-  document.getElementById('text-out').textContent +=`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)`;
+  document.getElementById('avancement').innerHTML =`${(percent * 100).toFixed(2)}% downloaded (${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)`;
 };
 
 function dl_track(playlist,element)
@@ -38,17 +36,25 @@ function dl_track(playlist,element)
     quality: 'highestaudio',
   });
   
-  //stream.on('error', console.error).on('progress', onProgress);
-  document.getElementById('text-out').textContent +="Téléchargement de : " + playlist.items[element].title + "\n";
+  stream.on('error', console.error).on('progress', onProgress);
+  document.getElementById('text-out').innerHTML +="Téléchargement de : " + playlist.items[element].title;
   let start = Date.now();
   
+  var metadata = [
+    `-metadata`,
+    `title=${playlist.items[element].title}  `,
+    `-metadata`,
+    `artist=${playlist.items[element].author.name}  `,
+  ];
+
   ffmpeg(stream)
     .audioBitrate(128)
+    .outputOptions(metadata)
     .save(`${path}/${playlist.title}/${playlist.items[element].title}.mp3`)
     .on('end', () => {
-      document.getElementById('text-out').textContent +=`\ndone - ${(Date.now() - start) / 1000}s - ${element+1}/${playlist.total_items}\n\n`;
+      document.getElementById('text-out').innerHTML +=` | Done - ${(Date.now() - start) / 1000}s - ${element+1}/${playlist.total_items} <br>`;
       if(element<(playlist.total_items-1))dl_track(playlist,++element);
-      else   document.getElementById('text-out').textContent +=`Playlist téléchargée en ${(Date.now() - startTime) / 1000}s\n`;
+      else   document.getElementById('text-out').innerHTML +=`Playlist téléchargée en ${(Date.now() - startTime) / 1000}s<br>`;
     });
 }
 
