f02bec2277609715578b5ed6c4c4fd133fd9ec21
asmirbelkic <asmir.belkic@gmail.com>
2020-08-09 22:32:38 +0200

    Front site update flexboxed everything

--- src/front.js
+++ src/front.js
@@ -1,64 +1,64 @@
-document.getElementById('min-button').addEventListener('click', function () {
+document.getElementById('min-button').addEventListener('click', function() {
   remote.getCurrentWindow().minimize()
 })
 
-document.getElementById('close-button').addEventListener('click', function () {
+document.getElementById('close-button').addEventListener('click', function() {
   remote.getCurrentWindow().close()
 })
 
-document.getElementById('accepte-rule').addEventListener('click', function () {
+document.getElementById('accepte-rule').addEventListener('click', function() {
   if (document.getElementById('c8').checked) {
-    document.getElementById('intro').classList.add('hidden')
-    document.getElementById('page').classList.remove('overlay')
+      document.getElementById('intro').classList.add('hidden')
+      document.getElementById('page').classList.remove('overlay')
   } else {
-    //changer la couleurs d'accepter les cgu en rouge ?
+      //changer la couleurs d'accepter les cgu en rouge ?
   }
 })
 
-document.getElementById('reject-rule').addEventListener('click', function () {
+document.getElementById('reject-rule').addEventListener('click', function() {
   remote.getCurrentWindow().close()
 })
 
 function showPop() {
   document.getElementById('notifOne').classList.add('visible')
   setTimeout(() => {
-    document.getElementById('notifOne').classList.remove('visible')
+      document.getElementById('notifOne').classList.remove('visible')
   }, 4000)
 }
 
 function showErr() {
   document.getElementById('notifErr').classList.add('visible')
   setTimeout(() => {
-    document.getElementById('notifErr').classList.remove('visible')
+      document.getElementById('notifErr').classList.remove('visible')
   }, 4000)
 }
 
 var checkBox = document.getElementById('checkDebug')
-checkBox.addEventListener('click', function () {
+checkBox.addEventListener('click', function() {
   if (checkBox.checked == true) {
-    document.querySelector('.is-debug').classList.add('block')
-    document.getElementById('debug-window').classList.add('block')
+      document.querySelector('.is-debug').classList.add('block')
+      document.getElementById('debug-window').classList.add('block')
   } else {
-    document.querySelector('.is-debug').classList.remove('block')
-    document.getElementById('debug-window').classList.remove('block')
+      document.querySelector('.is-debug').classList.remove('block')
+      document.getElementById('debug-window').classList.remove('block')
   }
 })
 
 /**
- * Contextual menu right click
- * no back-end or functionnality there
- **/
+* Contextual menu right click
+* no back-end or functionnality there
+**/
 
 var contextElement = document.getElementById('context-menu')
-window.addEventListener('contextmenu', function (event) {
+window.addEventListener('contextmenu', function(event) {
   event.preventDefault()
   contextElement.style.top = event.pageY + 'px'
   contextElement.style.left = event.pageX + 'px'
   contextElement.classList.add('active')
 })
-window.addEventListener('click', function () {
+window.addEventListener('click', function() {
   if (contextElement.classList.contains('active')) {
-    document.getElementById('context-menu').classList.remove('active')
+      document.getElementById('context-menu').classList.remove('active')
   }
 })
 
@@ -70,37 +70,37 @@ var btns = document.getElementsByClassName('open-card')
 
 // List all btns then on click remove all "visible" class and toggle them properly
 for (let i = 0; i < btns.length; i++) {
-  btns[i].onclick = function () {
-    for (var d = 0; d < modals.length; d++) {
-      modals[d].classList.remove('visible')
-    }
-    for (let x = 0; x < btns.length; x++) {
-      btns[x].classList.remove('active')
-    }
-    modals[i].classList.toggle('visible')
-    btns[i].classList.toggle('active')
+  btns[i].onclick = function() {
+      for (var d = 0; d < modals.length; d++) {
+          modals[d].classList.remove('visible')
+      }
+      for (let x = 0; x < btns.length; x++) {
+          btns[x].classList.remove('active')
+      }
+      modals[i].classList.toggle('visible')
+      btns[i].classList.toggle('active')
   }
 }
 
 /* 
-  TODO Back-End
-  .card-close class
-  [spans] & [modals] - [btns]
+TODO Back-End
+.card-close class
+[spans] & [modals] - [btns]
 */
 
 var spans = document.getElementsByClassName('card-close')
 for (let i = 0; i < spans.length; i++) {
-  document.addEventListener('keydown', function (event) {
-    const key = event.key // Or const {key} = event; in ES6+
-    if (key === 'Escape') {
-      // Do things
+  document.addEventListener('keydown', function(event) {
+      const key = event.key // Or const {key} = event; in ES6+
+      if (key === 'Escape') {
+          // Do things
+          modals[i].classList.remove('visible')
+          btns[i].classList.remove('active')
+      }
+  })
+  spans[i].onclick = function() {
       modals[i].classList.remove('visible')
       btns[i].classList.remove('active')
-    }
-  })
-  spans[i].onclick = function () {
-    modals[i].classList.remove('visible')
-    btns[i].classList.remove('active')
   }
 }
 
@@ -109,41 +109,41 @@ function initTabNav() {
   const tabContent = document.querySelectorAll('.content section')
 
   if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('active')
+      tabContent[0].classList.add('active')
 
-    function activeTab(index) {
-      tabContent.forEach(function (section) {
-        section.classList.remove('active')
-      })
-      tabContent[index].classList.add('active')
-    }
+      function activeTab(index) {
+          tabContent.forEach(function(section) {
+              section.classList.remove('active')
+          })
+          tabContent[index].classList.add('active')
+      }
 
-    tabMenu.forEach(function (itemMenu, index) {
-      itemMenu.addEventListener('click', function () {
-        activeTab(index)
-        var newTitle = itemMenu.dataset.title
-        document.getElementById('window-title').innerText = newTitle
+      tabMenu.forEach(function(itemMenu, index) {
+          itemMenu.addEventListener('click', function() {
+              activeTab(index)
+              var newTitle = itemMenu.dataset.title
+              document.getElementById('window-title').innerText = newTitle
+          })
       })
-    })
   }
 }
 
 initTabNav()
 
-document.getElementById('cgu-see').addEventListener('click', function (e) {
+document.getElementById('cgu-see').addEventListener('click', function(e) {
   document.getElementById('cgu-more').classList.toggle('block')
 })
 
 var dropdown = document.getElementsByClassName('dropdown-btn')
 
 for (var i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-    var dropdownContent = this.nextElementSibling
-    if (dropdownContent.style.display === 'block') {
-      dropdownContent.style.display = 'none'
-    } else {
-      dropdownContent.style.display = 'block'
-    }
+  dropdown[i].addEventListener('click', function() {
+      this.classList.toggle('active')
+      var dropdownContent = this.nextElementSibling
+      if (dropdownContent.style.display === 'block') {
+          dropdownContent.style.display = 'none'
+      } else {
+          dropdownContent.style.display = 'block'
+      }
   })
 }
--- src/ytpl2mp3.js
+++ src/ytpl2mp3.js
@@ -2,12 +2,14 @@ const fs = require('fs')
 const ytpl = require('ytpl')
 const ytdl = require('ytdl-core')
 const ffmpeg = require('fluent-ffmpeg')
-const { remote } = require('electron')
+const {
+    remote
+} = require('electron')
 const path = require('path')
 
 var pathToFfmpeg = require('ffmpeg-static').replace(
-  'app.asar',
-  'app.asar.unpacked',
+    'app.asar',
+    'app.asar.unpacked',
 )
 ffmpeg.setFfmpegPath(pathToFfmpeg)
 
@@ -16,156 +18,174 @@ var taille
 var chemin
 
 window.onload = () => {
-  let setting = JSON.parse(fs.readFileSync(__dirname + '/userSetting.json'))
+    let setting = JSON.parse(fs.readFileSync(__dirname + '/userSetting.json'))
 
-  chemin = setting.path || process.env.HOME || process.env.USERPROFILE
-  document.getElementById('path_text').value = chemin
+    chemin = setting.path || process.env.HOME || process.env.USERPROFILE
+    document.getElementById('path_text').value = chemin
 
-  if (setting.welcomeMsg) {
-    document.getElementById('s3').checked = true
-    document.getElementById('intro').classList.remove('hidden')
-  } else {
-    document.getElementById('s3').checked = false
-  }
+    if (setting.welcomeMsg) {
+        document.getElementById('s3').checked = true
+        document.getElementById('page').classList.add('active')
+    } else {
+        document.getElementById('s3').checked = false
+    }
 
-  if (setting.debug){
-    document.getElementById('checkDebug').checked = true
-    document.querySelector('.is-debug').classList.add('block')
-    document.getElementById('debug-window').classList.add('block')
-  } else document.getElementById('checkDebug').checked = false
+    if (setting.debug) {
+        document.getElementById('checkDebug').checked = true
+        document.querySelector('.is-debug').classList.add('block')
+        document.getElementById('debug-window').classList.add('block')
+    } else document.getElementById('checkDebug').checked = false
 
-  if(setting.dark) document.getElementById('s5').checked = true
+    if (setting.dark) document.getElementById('s5').checked = true
 
-  document.getElementById(setting.format).checked = true;
+    document.getElementById(setting.format).checked = true
 }
 
 function download() {
-  startTime = Date.now()
-  let link = document.getElementById('playlist-id').value
-  ytpl.getPlaylistID(link, (err, playlistID) => {
-    if (err) {
-      try {
-        if (ytdl.getVideoID(link)) return download_track(link)
-      } catch (err) {
-        return afficher_err('Wrong link', err)
-      }
-    }
+    startTime = Date.now()
+    let link = document.getElementById('playlist-id').value
+    ytpl.getPlaylistID(link, (err, playlistID) => {
+        if (err) {
+            try {
+                if (ytdl.getVideoID(link)) return download_track(link)
+            } catch (err) {
+                return afficher_err('Wrong link', err)
+            }
+        }
 
-    ytpl(playlistID, { limit: Infinity }, function (err, playlist) {
-      if (err) return afficher_err(err.name, err.message)
-      document.getElementById('text-out').innerHTML ='Téléchargement de la playlist : ' + playlist.title + '<br>'
-      document.getElementById('text-out').innerHTML +='Nombre de piste audio à télécharger : ' +playlist.total_items +'<br><br>'
-      if (!fs.existsSync(chemin + '/' + playlist.title))
-        fs.mkdirSync(chemin + '/' + playlist.title)
-      dl_track_from_playlist(playlist, 0)
+        ytpl(playlistID, {
+            limit: Infinity
+        }, function(err, playlist) {
+            if (err) return afficher_err(err.name, err.message)
+            document.getElementById('text-out').innerHTML =
+                'Téléchargement de la playlist : ' + playlist.title + '<br>'
+            document.getElementById('text-out').innerHTML +=
+                'Nombre de piste audio à télécharger : ' +
+                playlist.total_items +
+                '<br><br>'
+            if (!fs.existsSync(chemin + '/' + playlist.title))
+                fs.mkdirSync(chemin + '/' + playlist.title)
+            dl_track_from_playlist(playlist, 0)
+        })
     })
-  })
 }
 
 const onProgress = (chunkLength, downloaded, total) => {
-  taille = (total / 1024 / 1024).toFixed(2)
-  const percent = downloaded / total
-  //const bar = document.getElementById('myBar')
-  const bartxt = document.getElementById('progressText')
-  //bar.style.width = (percent * 100).toFixed(2) + '%'
-  bartxt.innerHTML = (percent * 100).toFixed(2) + '%'
-  if ((percent * 100).toFixed(2) == 100) {
-    bartxt.innerHTML = 'Téléchargement terminé !'
-  }
+    taille = (total / 1024 / 1024).toFixed(2)
+    const percent = downloaded / total
+    //const bar = document.getElementById('myBar')
+    const bartxt = document.getElementById('progressText')
+    //bar.style.width = (percent * 100).toFixed(2) + '%'
+    bartxt.innerHTML = (percent * 100).toFixed(2) + '%'
+    if ((percent * 100).toFixed(2) == 100) {
+        bartxt.innerHTML = 'Téléchargement terminé !'
+    }
 }
 
 function download_track(link) {
-  let stream = ytdl(link, { quality: 'highestaudio' })
+    let stream = ytdl(link, {
+        quality: 'highestaudio'
+    })
 
-  stream.on('progress', onProgress).on('info', (info) => {
-    //document.getElementById('progressBar').classList.add('visible')
-    document.getElementById('text-out').innerHTML +="Téléchargement de : " + info.videoDetails.title;
-    let start = Date.now()
+    stream.on('progress', onProgress).on('info', (info) => {
+        //document.getElementById('progressBar').classList.add('visible')
+        document.getElementById('text-out').innerHTML +=
+            'Téléchargement de : ' + info.videoDetails.title
+        let start = Date.now()
 
-    var metadata = [
-      `-metadata`,
-      `title=${info.videoDetails.title}  `,
-      `-metadata`,
-      `artist=${info.videoDetails.author.name}  `,
-    ]
+        var metadata = [
+            `-metadata`,
+            `title=${info.videoDetails.title}  `,
+            `-metadata`,
+            `artist=${info.videoDetails.author.name}  `,
+        ]
 
-    ffmpeg(stream)
-      .audioBitrate(128)
-      .outputOptions(metadata)
-      .save(path.normalize(`${chemin}/${info.videoDetails.title}.mp3`))
-      .on('error', function (err, stdout, stderr) {
-        afficher_err(info.videoDetails.title, err)
-      })
-      .on('end', () => {
-        afficher_notif(info.videoDetails.title, taille + 'MB')
-      })
-  })
+        ffmpeg(stream)
+            .audioBitrate(128)
+            .outputOptions(metadata)
+            .save(path.normalize(`${chemin}/${info.videoDetails.title}.mp3`))
+            .on('error', function(err, stdout, stderr) {
+                afficher_err(info.videoDetails.title, err)
+            })
+            .on('end', () => {
+                afficher_notif(info.videoDetails.title, taille + 'MB')
+            })
+    })
 }
 
 function dl_track_from_playlist(playlist, element) {
-  let stream = ytdl(playlist.items[element].url_simple, {
-    quality: 'highestaudio',
-  })
-
-  stream.on('progress', onProgress)
-  //document.getElementById('progressBar').classList.add('visible')
-  document.getElementById('text-out').innerHTML +="Téléchargement de : " + playlist.items[element].title;
-  let start = Date.now()
-
-  var metadata = [
-    `-metadata`,
-    `title=${playlist.items[element].title}  `,
-    `-metadata`,
-    `artist=${playlist.items[element].author.name}  `,
-  ]
-
-  ffmpeg(stream)
-    .audioBitrate(128)
-    .outputOptions(metadata)
-    .save(`${chemin}/${playlist.title}/${playlist.items[element].title}.mp3`)
-    .on('error', function (err, stdout, stderr) {
-      afficher_err(info.videoDetails.title, err)
-      if (element < playlist.total_items - 1)
-        dl_track_from_playlist(playlist, ++element)
-      else
-        document.getElementById('text-out').innerHTML += `Playlist téléchargée en ${(Date.now() - startTime) / 1000}s<br>`
+    let stream = ytdl(playlist.items[element].url_simple, {
+        quality: 'highestaudio',
     })
-    .on('end', () => {
-      afficher_notif(playlist.items[element].title, taille + ' MB')
-      if (element < playlist.total_items - 1)
-        dl_track_from_playlist(playlist, ++element)
-      else
-        document.getElementById('text-out',).innerHTML += `Playlist téléchargée en ${(Date.now() - startTime) / 1000}s<br>`
-    })
+
+    stream.on('progress', onProgress)
+    //document.getElementById('progressBar').classList.add('visible')
+    document.getElementById('text-out').innerHTML +=
+        'Téléchargement de : ' + playlist.items[element].title
+    let start = Date.now()
+
+    var metadata = [
+        `-metadata`,
+        `title=${playlist.items[element].title}  `,
+        `-metadata`,
+        `artist=${playlist.items[element].author.name}  `,
+    ]
+
+    ffmpeg(stream)
+        .audioBitrate(128)
+        .outputOptions(metadata)
+        .save(`${chemin}/${playlist.title}/${playlist.items[element].title}.mp3`)
+        .on('error', function(err, stdout, stderr) {
+            afficher_err(info.videoDetails.title, err)
+            if (element < playlist.total_items - 1)
+                dl_track_from_playlist(playlist, ++element)
+            else
+                document.getElementById(
+                    'text-out',
+                ).innerHTML += `Playlist téléchargée en ${
+          (Date.now() - startTime) / 1000
+        }s<br>`
+        })
+        .on('end', () => {
+            afficher_notif(playlist.items[element].title, taille + ' MB')
+            if (element < playlist.total_items - 1)
+                dl_track_from_playlist(playlist, ++element)
+            else
+                document.getElementById('text-out', ).innerHTML += `Playlist téléchargée en ${
+          (Date.now() - startTime) / 1000
+        }s<br>`
+        })
 }
 
 function choose_path() {
-  var windows = remote.getCurrentWindow()
-  var data = remote.dialog.showOpenDialogSync(windows, {properties: ['openDirectory']})
-  if (data != undefined) chemin = data
-  document.getElementById('path_text').value = chemin
+    var windows = remote.getCurrentWindow()
+    var data = remote.dialog.showOpenDialogSync(windows, {
+        properties: ['openDirectory'],
+    })
+    if (data != undefined) chemin = data
+    document.getElementById('path_text').value = chemin
 }
 
 function afficher_err(text, err) {
-  document.getElementById('errp').textContent = text
-  document.getElementById('errs').textContent = err
-  //showErr()
+    document.getElementById('errp').textContent = text
+    document.getElementById('errs').textContent = err
+    //showErr()
 }
 
 function afficher_notif(text1, text2) {
-  document.getElementById('notifp').textContent = text1
-  document.getElementById('notifs').textContent = text2
-  //showPop()
+    document.getElementById('notifp').textContent = text1
+    document.getElementById('notifs').textContent = text2
+    //showPop()
 }
 
 function save_param() {
-  let setting = {
-    format: document.querySelector('input[name="radio"]:checked').id,
-    path: document.getElementById('path_text').value,
-    welcomeMsg: document.getElementById('s3').checked,
-    dark: document.getElementById('s5').checked,
-    debug: document.getElementById('checkDebug').checked,
-  }
+    let setting = {
+        format: document.querySelector('input[name="radio"]:checked').id,
+        path: document.getElementById('path_text').value,
+        welcomeMsg: document.getElementById('s3').checked,
+        dark: document.getElementById('s5').checked,
+        debug: document.getElementById('checkDebug').checked,
+    }
 
-  fs.writeFileSync(__dirname + '/userSetting.json', JSON.stringify(setting))
+    fs.writeFileSync(__dirname + '/userSetting.json', JSON.stringify(setting))
 }
